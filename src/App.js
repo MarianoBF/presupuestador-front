373228d161879a56404231ca90a0fdba440167de
MarianoBF <mariano.b.fernandez@gmail.com>
2021-03-05 16:53:33 -0300

    inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+function handler(){
+  console.log(traer())
+}
+
+  async function traer() {
+    const respuesta = await axios.post("http://localhost:3000/usuario",{
+      nombre: "Juan",
+      apellido: "Perez"
+    })
+    return respuesta
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handler}>Prueba</button>
     </div>
   );
 }
