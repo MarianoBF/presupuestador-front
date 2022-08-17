3190eeac514d3e0efbbea5e742d8c774afe58fbf
Mariano <mariano.b.fernandez@gmail.com>
2022-08-17 19:19:08 -0300

    auth header in intercept

--- src/http-common.js
+++ src/http-common.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 
-let token = sessionStorage.getItem("pre_ejisao") || "";
-
 const http = axios.create({
   baseURL: process.env.REACT_APP_BASE_API_URL,
   headers: {
     "Content-type": "application/json",
-    "x-access-token": "" + token,
   },
 });
 
+http.interceptors.request.use((req) => {
+  let token = sessionStorage.getItem("pre_ejisao") || "";
+  req.headers.common["x-access-token"] = token;
+  return req;
+});
 
-  http.interceptors.response.use(
-    res => res,
-    (error) => {
-      if (error?.response?.status === 401) {
-        window.location.href = '/';
-      }
+http.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error?.response?.status === 401) {
+      window.location.href = "/";
     }
-  );
-
+  }
+);
 
 export default http;
